e89df304fcf4eef910c50c6b5c32aa45e59d0a22
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-03-17 15:56:07 -0300

    novos exer

--- testes.js/ex010.js
+++ testes.js/ex010.js
@@ -1,7 +1,19 @@
-var idade = 22
-if (idade < 18) {
-    console.log ('Menor de idade')
-} 
-else {
-    console.log ('Maior de idade')
+var agora = new Date()
+//Tem que ter isso junto
+var hora = agora.getHours() 
+//Para pegarmos a hora real do computador
+
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora <= 5) {
+    console.log('Boa madrugada.')
 }
+else if (hora < 12) {
+    console.log('Bom dia.')
+}
+else if (hora <= 17) {
+    console.log('Boa tarde.')
+}
+else if (hora > 0){
+    console.log('Boa noite.')
+}
+
