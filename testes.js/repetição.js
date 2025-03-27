b8106c7bc0faac28d3b4ae9b492dc1780be30a8f
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-03-27 17:27:41 -0300

    novos exerc

---
+++ testes.js/repetição.js
@@ -0,0 +1,15 @@
+
+/* Laço de Repetição
+var c = 1
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+//Executa o (DO) e depois faz o laço de repetição ou não
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
