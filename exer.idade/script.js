4bc8f3e4238187a8969db075e00e94696b48973e
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-03-27 15:51:01 -0300

    feito

--- exer.idade/script.js
+++ exer.idade/script.js
@@ -8,7 +8,45 @@ function verificar() {
        alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var sexo = document.getElementsByName('radsex')
-        var idade = ano - fano.value
-        res.innerHTML = 
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement ('img')
+        img.setAttribute('id','foto')
+        
+        if(sexo[0].checked){
+            genero = 'Homem'
+            if (idade>=0 && idade< 14){
+                //criança
+                img.setAttribute('src','foto.bebeho.jpg')
+            } else if(idade <22){
+                //jovem
+                img.setAttribute('src','foto.jovemho.jpg')
+            } else if (idade<55){
+                //adulto
+                img.setAttribute('src','foto.adulto.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src','foto.velho.jpg')
+            }
+        } 
+        else if(sexo[1].checked){
+            genero = 'Mulher'
+            if (idade>=0 && idade< 14){
+                //criança
+                img.setAttribute('src','foto.bebemu.jpg')
+            } else if(idade <22){
+                //jovem
+                img.setAttribute('src','foto.jovemmu.jpg')
+            } else if (idade<55){
+                //adulto
+                img.setAttribute('src','foto.adulta.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src','foto.velha.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
