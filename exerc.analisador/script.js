b501649eedfdca7e96f554fdd7a3d7cf17d1a039
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-04-24 16:01:31 -0300

    novods

--- exerc.analisador/script.js
+++ exerc.analisador/script.js
@@ -1,9 +1,9 @@
-var num = document.getElementById('num')
-var lista = document.getElementById('lista')
+var num = document.querySelector('input#num')
+var lista = document.querySelector('select#lista')
 var res = document.querySelector('div#res')
 var valores = []
 
-function isNum(n){
+function isNumero(n){
     if(Number(n) >= 1 && Number(n) <=100){
         return true
     } else {
@@ -11,8 +11,8 @@ function isNum(n){
     }
 }
 
-function inlista(n, l){
-    if (l.indexOf(Number(n) != -1)){
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
         return true
     } else {
         return false
@@ -20,20 +20,43 @@ function inlista(n, l){
 }
 
 function adicionar(){
-    if(isNum(num.value) && !inlista(num.value)) {
-       
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+       valores.push(Number(num.value))
+       var item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado.`
+       lista.appendChild(item)
+       res.innerHTML = ''
     } else{
         window.alert('Valor inválido ou já encontrado na Lista.')
     }
-    
-    
-    
-    
-    
-    /*if(num.value == 0 || num.value > 100){
-        window.alert('Valor inválido ou já encontrado na Lista.')
-    } else{
-        
-    } */
-
+    num.value = ''
+    num.focus() 
+    //Para a barra de escrita zerar ao ser adicionado.
 }
+
+function finalizar() {
+    if (valores.length == 0){
+        alert("Adicione valores antes de finalizar")
+    } else{
+        var total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        } 
+        media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p> `
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p> `
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p> ` 
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p> ` 
+        res.innerHTML += `<p>A média de todos os valores é ${media}.</p> ` 
+    } 
+ } 
