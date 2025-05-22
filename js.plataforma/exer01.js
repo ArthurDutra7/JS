e65d7eec3c9e7e9f4e35ee0454b9fd0d2e48984a
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-05-22 17:34:21 -0300

    novos

--- js.plataforma/exer01.js
+++ js.plataforma/exer01.js
@@ -1,6 +1,15 @@
-const numero =10;
+const etanol = 3.15
+const gasolina = 5.70
+const litro = 8 //8 litros para cada km
+const km = 260
+const tipocombu = 'Gasolina'
 
-const numeropar = numero % 3 == 0;
+const consumo = km / litro
 
-console.log(numeropar);
-
+if(tipocombu === 'etanol'){
+    const valor = consumo * etanol
+    console.log('O valor que será gasto se for escolhido Etanol será de:',valor)
+} else{
+    const valor = consumo * gasolina
+    console.log('O valor que será gasto se for escolhido Gasolina será de:',valor)
+}
