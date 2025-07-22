65d1e3c5e20741a05f42ab333fbb197be3cbf54e
Arthur Dutra <a4thursouzaxx@gmail.com>
2025-07-21 21:44:42 -0300

    novos

--- testes.js/vetores/vetor2.js
+++
@@ -1,9 +0,0 @@
-//USANDO O (FOR)
-
-var num = [1,3,6,8]
-
-num.sort()
-
-for(var pos=0;pos<num.length;pos++){
-    console.log(`Mostre os números do vetor: ${num[pos]}`)
-}
--- testes.js/vetores/vetor3.js
+++
@@ -1,9 +0,0 @@
-//UTILIZANDO O (FOR) COM (IN)
-
-var num = [1,7,3,9]
-
-num.sort() // Ordem númerica do menor até o maior.
-
-for(var pos in num){
-    console.log(`Mostre os números do vetor: ${num[pos]}`)
-}
